dd682445fabae98026c02c5b274733ba84b85e1c
AbramBeazer <abrambeazer@gmail.com>
2018-03-15 19:38:31 -0600

    Added some functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
- 
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    party: [],
+    size: 0,
+    newName: "",
+    newClass: "",
+    newStr: "",
+    newDex: "",
+    newCon: "",
+    newInt: "",
+    newWis: "",
+    newCha: "",
+  },
+  created: function() {
+//
+  },
+  methods: {
+    addCharacter: function() {
+
+    },
+    removeCharacter: function() {
+
+    },
+    clearParty: function() {
+
+    },
+  }
+});
